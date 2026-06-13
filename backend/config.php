c3ef16805fc33da329f7eab509a9747fdcd65599
DREAMI1590 <ilyassathedreami1509@gmail.com>
2026-06-13 11:18:35 +0700

    Update database connection settings for Docker

--- backend/config.php
+++ backend/config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "sql211.infinityfree.com";
-$username = "if0_40042420";
-$password = "C8apLwQUNqa";
-$dbname = "if0_40042420_db_swiftmeal";
+$servername = "db";                   // Nama service di docker-compose
+$username = "swiftmeal_user";         // User di docker-compose
+$password = "SwiftMeal123!";          // Password di docker-compose
+$dbname = "db_swiftmeal";             // Nama database di docker-compose
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
